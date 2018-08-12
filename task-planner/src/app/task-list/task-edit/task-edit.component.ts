6b4e95f0b082441145d32727e9af8469aae972f5
Alexander Eremeev <eremeev@gmail.com>
2018-08-12 09:52:20 +0300

    task 64253 component life cycle

--- task-planner/src/app/task-list/task-edit/task-edit.component.ts
+++ task-planner/src/app/task-list/task-edit/task-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
 import { Task } from '../task.model';
 
 @Component({
@@ -6,14 +6,26 @@ import { Task } from '../task.model';
   templateUrl: './task-edit.component.html',
   styleUrls: ['./task-edit.component.css']
 })
-export class TaskEditComponent implements OnInit {
+export class TaskEditComponent implements OnInit, OnDestroy, OnChanges {
   @Output() completeEditEmitter = new EventEmitter<Task>();
   @Input() task: Task;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('initializing edit-task component')
   }
+  ngOnDestroy() {
+    console.log('destroying edit-task component')
+  }
+  ngOnChanges() {
+    console.log('task to edit changed, entering edit mode for task: ' + this.task);
+    if (this.task.status == 'Выполнено') {
+      alert('Выполненные задачи не подлежат редактированию!');
+      this.cancelEdit();
+    }
+  }
+
 
   editTask() {
     this.completeEditEmitter.emit(this.task);
