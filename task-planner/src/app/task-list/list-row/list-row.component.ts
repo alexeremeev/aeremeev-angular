a5fa3537b644e37cdea2b23f462c8362916697cf
Alexander Eremeev <eremeev@gmail.com>
2018-08-20 19:50:59 +0300

    task 65504 dependency injection

--- task-planner/src/app/task-list/list-row/list-row.component.ts
+++ task-planner/src/app/task-list/list-row/list-row.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HelperService } from '../../shared/services/helper.service';
 
 @Component({
+  providers: [HelperService],
   selector: '[app-list-row]',
   templateUrl: './list-row.component.html',
   styleUrls: ['./list-row.component.css']
 })
 export class ListRowComponent implements OnInit {
-  helperService = new HelperService();
+
 
   @Output() deleteTaskEmitter = new EventEmitter<string>();
   @Output() editTaskEmitter = new EventEmitter<string>();
@@ -18,7 +19,7 @@ export class ListRowComponent implements OnInit {
   @Input() dateEnd: string;
   @Input() status: string;
 
-  constructor() { }
+  constructor(private helperService: HelperService) { }
 
   ngOnInit() {
   }
--- task-planner/src/app/task-list/statistic/statistic.component.ts
+++ task-planner/src/app/task-list/statistic/statistic.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HelperService } from '../../shared/services/helper.service';
 @Component({
+  providers: [HelperService],
   selector: 'app-statistic',
   templateUrl: './statistic.component.html',
   styleUrls: ['./statistic.component.css']
 })
 export class StatisticComponent implements OnInit {
 
+  currentDate: string;
+
   @Input() tasksAll;
   @Input() tasksPlanned;
   @Input() tasksOverdue;
   @Input() tasksFinished;
 
-  constructor() { }
+  constructor(private helperService: HelperService) {
+    let date = new Date();
+    this.currentDate = date.toISOString();
+  }
 
   ngOnInit() {
   }
